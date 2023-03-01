c81ab50a0456443286f51bf53fdb8f46ea86df3a
Valerio Pinsone <valerio.pinsone@gmail.com>
2023-03-01 22:13:30 +0100

    collegamento backend locale

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,25 +1,41 @@
 import React , { useState } from "react";
 import styles from '@/styles/Home.module.css'
 
+//function to post increment to backend
+const postQuantity = (id: number, q: number) => {
+    fetch('http://localhost:3001/products/setquantity/' + id+ '/' + q)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
+}
+
 const Card: React.FC<any> = ({product}) => {
-    const [quantity, setQuantity] = useState(product.quantity);
+    const [quantity, setQuantity] = useState(product.product_quantity);
     return (
         <>
         <div className={styles.card}>
         <div className={(quantity >3 ? styles.quantityBarBlue : styles.quantityBarRed)}></div>
         <div className={styles.quantity}>{quantity}</div>
         <div className={styles.center}>
-            <h3>{product.name}</h3>
+            <h3>{product.product_name}</h3>
             </div>
             <hr className={styles.hr}></hr>
             <div className={styles.center}>
-            <textarea className={styles.textarea} defaultValue={product.desc} readOnly></textarea>
+            <textarea className={styles.textarea} defaultValue={product.product_desc} readOnly></textarea>
             <button className={styles.cardButtonRed}  onClick={function(){
                 if(quantity > 0){
                     setQuantity(quantity - 1);
+                    postQuantity(product.product_id, quantity -1);
                 }
             }} >-</button>
-            <button className={styles.cardButtonGreen} onClick={() => setQuantity(quantity + 1)}>+</button>
+            <button className={styles.cardButtonGreen} onClick={function() {
+                setQuantity(quantity + 1);
+                postQuantity(product.product_id, quantity +1);
+                }}>+</button>
         </div>
         </div>
         </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,122 +8,35 @@ import Card from '@/components/Card'
 
 const inter = Inter({ subsets: ['latin'] })
 
-let products = [
-  {
-    id: 1,
-    name: "Tubo Plastica",
-    desc: "Tubo di plastica da 1.5m",
-    quantity: 58,
-  },
-  {
-    id: 2,
-    name: "Bullone 30mm",
-    desc: "Bullone da 30mm",
-    quantity: 2,
-  },
-  {
-    id: 3,
-    name: "Guaina Doppio Strato",
-    desc: "Guaina doppio strato da 1.5m",
-    quantity: 3,
-  },
-  {
-    id: 4,
-    name: "Centralina 12V per cancelli",
-    desc: "Centralina 12V per cancelli",
-    quantity: 12,
-  },
-  {
-    id: 5,
-    name: "Cavo 2x1.5mm",
-    desc: "Cavo 2x1.5mm",
-    quantity: 78,
-  },
-  {
-    id: 6,
-    name: "Cavo 2x2.5mm",
-    desc: "Cavo 2x2.5mm",
-    quantity: 45,
-  },
-  {
-    id: 7,
-    name: "Cavo 2x4mm",
-    desc: "Cavo 2x4mm",
-    quantity: 1,
-  },
-  {
-    id: 8,
-    name: "Piattina 1x1",
-    desc: "Piattina 1x1",
-    quantity: 56,
-  },
-  {
-    id: 9,
-    name: "Piattina 1x2",
-    desc: "Piattina 1x2",
-    quantity: 34,
-  },
-  {
-    id: 10,
-    name: "Set Chiavi",
-    desc: "Set Chiavi",
-    quantity: 2,
-  },
-  {
-    id: 11,
-    name: "Cavo 2x6mm",
-    desc: "Cavo 2x6mm",
-    quantity: 12,
-  },
-  {
-    id: 12,
-    name: "Tubo PVC 50mm",
-    desc: "Tubo PVC 50mm",
-    quantity: 12,
-  },
-  {
-    id: 13,
-    name: "Tubo PVC 75mm",
-    desc: "Tubo PVC 75mm",
-    quantity: 0,
-  },
-  {
-    id: 14,
-    name: "Tubo PVC 100mm",
-    desc: "Tubo PVC 100mm",
-    quantity: 12,
-  },
-  {
-    id: 15,
-    name: "Plexiglass 1.5m",
-    desc: "Plexiglass 1.5m",
-    quantity: 12,
-  },
-  {
-    id: 16,
-    name: "Plexiglass 2m",
-    desc: "Plexiglass 2m",
-    quantity: 12,
-  }
-];
-
-let productsFiltered = products;
 let filtro = "prova";
 export default function Home() {
+  
 
-
+  const [products, setProducts] = useState([]);
   const [search, setSearch] = useState("");
   const callback = (payload: any) => {
     setSearch(payload)
   }
 
   useEffect(() => {
-    filtro = search;
+    filtro = search; //filtro tornato dalla searchbar (da usare in nella chiamata be)
    /*  console.log(filtro); */
     //filter the products
 /*     productsFiltered = products.filter((product) => {
       return product.name.toLowerCase().includes(filtro.toLowerCase());
-    }); */
+    });
+ */
+
+    
+    fetch('http://localhost:3001/products/' + filtro)
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            setProducts(data);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
   }
   , [search]);
 
@@ -136,16 +49,20 @@ export default function Home() {
         <meta name="description" content="App Inventario" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"></link>
       </Head>
      <Navbar callback={callback}/>
+      <div className={styles.buttonContainer}>
+        <button className={styles.buttonAddNew}><span className={["material-icons", styles.addicon].join(' ')}>add_circle</span>Aggiungi Nuovo</button>
+      </div>
+     
       <div className={styles.cardContainer}>
 
-   
       
-      
-       {productsFiltered.map((products, filtro) => (
-        
-          <Card key={products.id} product={products}/>
+       {products.map((products:any) => (
+         
+          <Card key={products.product_id} product={products}/>
         ))}
       </div>
     
